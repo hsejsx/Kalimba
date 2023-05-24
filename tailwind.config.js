aeff999946b2dfddd8f258b229a8c8be5dc10c4b
hsejsx <hsejsx@gmail.com>
2023-05-24 14:30:53 +0900

    primary color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#52057B',
+      },
+    },
   },
   plugins: [],
 };
