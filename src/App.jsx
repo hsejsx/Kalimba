798389182faa7bffe3b18651e9fd4ad94a1153f1
hsejsx <hsejsx@gmail.com>
2023-05-24 22:51:30 +0900

    react query 설치 및 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthProvider } from './context/AuthContext';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <section className='w-full max-w-7xl pb-4 mx-auto'>
-      <AuthProvider>
-        <Navbar />
-        <Outlet />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Navbar />
+          <Outlet />
+        </AuthProvider>
+      </QueryClientProvider>
     </section>
   );
 }
