2a0ed209944ea824367ee857b9da6e2ae3ba31a6
hsejsx <hsejsx@gmail.com>
2023-05-24 14:31:46 +0900

    root 및 app 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <section className='w-full max-w-7xl pb-4 mx-auto'>
       <Navbar />
       <Outlet />
-    </>
+    </section>
   );
 }
 
