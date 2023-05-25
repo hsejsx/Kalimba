3751f95ef61fab463ca487cecde5af891fce5a84
hsejsx <hsejsx@gmail.com>
2023-05-25 11:46:13 +0900

    Edit, EditForm 마크업 및 스타일

---
+++ src/components/edit/EditForm.jsx
@@ -0,0 +1,90 @@
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { writePost } from '../../api/firebase';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
+
+export default function EditForm() {
+  // TODO : 로직 분리하기
+  const path = useLocation().state.slice(1);
+  const queryClient = useQueryClient();
+  const defaultValue = path ? path : 'music';
+  const { user } = useContext(AuthContext);
+  const [post, setPost] = useState({
+    title: '',
+    category: defaultValue,
+    content: '',
+  });
+  const [result, setResult] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setPost(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    writePost(user, post).then(() => {
+      setResult(true);
+      queryClient.refetchQueries('posts');
+      setTimeout(() => {
+        navigate(`/${post.category}`);
+      }, 2000);
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='w-full mt-4'>
+      {result && <p className='text-2xl font-semibold'>발행 완료</p>}
+      <input
+        onChange={handleChange}
+        className='w-6/12 p-2 text-2xl mb-4 border-b-2 outline-none'
+        type='text'
+        name='title'
+        required
+        value={post.title}
+        placeholder='제목을 입력하세요'
+      />
+      <div className='w-full border-2 border-b-0'>
+        <select
+          onChange={handleChange}
+          defaultValue={defaultValue}
+          className='border-r-2'
+          name='category'
+          id='category'
+        >
+          <option value='music'>악보</option>
+          <option value='knowhow'>노하우</option>
+        </select>
+      </div>
+      <textarea
+        onChange={handleChange}
+        className='w-full h-[50vh] p-2 mb-4 border-2 appearance-none resize-none outline-none'
+        name='content'
+        value={post.content}
+        required
+        placeholder='내용을 입력하세요'
+      />
+      <div className='w-full text-center'>
+        <button
+          onClick={handleGoBack}
+          className='inline-flex items-center justify-center w-24 py-2 mr-2 text-white bg-red-500'
+          type='button'
+        >
+          취소
+        </button>
+        <button
+          className='inline-flex items-center justify-center w-24 py-2 text-white bg-blue-500'
+          type='submit'
+        >
+          등록
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/pages/Edit.jsx
@@ -0,0 +1,10 @@
+import Container from '../components/common/Container';
+import EditForm from '../components/edit/EditForm';
+
+export default function Edit() {
+  return (
+    <Container>
+      <EditForm />
+    </Container>
+  );
+}
