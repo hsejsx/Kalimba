784b55dae0a2721805928f88372e3b72c2dca42b
hsejsx <hsejsx@gmail.com>
2023-05-25 11:52:46 +0900

    Music, Knowhow 페이지 마크업

---
+++ src/pages/Music.jsx
@@ -0,0 +1,20 @@
+import Container from '../components/common/Container';
+import Table from '../components/common/Table';
+import Title from '../components/common/Title';
+import WriteBtn from '../components/common/WriteBtn';
+import useFetchPosts from '../hooks/useFetchPosts';
+
+export default function Music() {
+  const [isLoading, error, posts] = useFetchPosts();
+
+  if (isLoading) return <p>로딩중...</p>;
+  if (error) return <p>에러 발생 ㅠ_ㅠ...</p>;
+  if (posts)
+    return (
+      <Container>
+        <Title title='악보 게시판' />
+        <WriteBtn />
+        <Table posts={posts} />
+      </Container>
+    );
+}
