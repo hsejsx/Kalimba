b5017e4b44e8b32f5d996c40ee36b00693bd6992
hsejsx <hsejsx@gmail.com>
2023-05-25 17:46:06 +0900

    로그인 했을 때만 글쓰기 버튼 활성화 설정

--- src/components/common/WriteBtn.jsx
+++ src/components/common/WriteBtn.jsx
@@ -1,16 +1,20 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function WriteBtn() {
   const path = useLocation().pathname;
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
   const handleClick = () => {
     navigate('/edit', { state: path });
   };
   return (
     <button
       onClick={handleClick}
-      className='self-end flex items-center justify-center w-24 py-2 px-3 mb-4 text-white bg-black hover:opacity-80'
+      className='self-end flex items-center justify-center w-24 py-2 px-3 mb-4 text-white bg-black hover:opacity-80 disabled:bg-gray-500'
       type='button'
+      disabled={!user}
     >
       글쓰기
     </button>
