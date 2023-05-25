201d9d1ef6ccf35ee61dd9d80e95dd15cbb5b784
hsejsx <hsejsx@gmail.com>
2023-05-25 11:48:43 +0900

    WriteBtn 상태 전달 추가

--- src/components/common/WriteBtn.jsx
+++ src/components/common/WriteBtn.jsx
@@ -1,14 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-export default function WriteBtn({ onClick }) {
+export default function WriteBtn() {
+  const path = useLocation().pathname;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/edit', { state: path });
+  };
   return (
-    <Link
-      to='/edit'
-      onClick={onClick}
+    <button
+      onClick={handleClick}
       className='self-end flex items-center justify-center w-24 py-2 px-3 mb-4 text-white bg-black hover:opacity-80'
       type='button'
     >
       글쓰기
-    </Link>
+    </button>
   );
 }
